7873b17cdc947208dc09424b5971c00ccc0a95c4
MariaSarbaj <marysarbaj@gmail.com>
2022-02-11 20:35:25 +0200

    Исправляет файл с тестом

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import App from './app';
 
 test('Renders app-component', () => {
-  render(<App offersNumber={0} />);
-  const textElement = screen.getByText(/Hello, world!/i);
+  render(<App offersNumber={312}/>);
+  const textElement = screen.getByText(/312 places to stay in Amsterdam/i);
   expect(textElement).toBeInTheDocument();
 });
